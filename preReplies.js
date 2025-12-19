13dbef2840a18351ed9f82d6217e37ee9a21b786
yoursPrachi <yogeshbalajiearth@gmail.com>
2025-12-19 23:10:08 +0530

    Create preReplies.js

---
+++ preReplies.js
@@ -0,0 +1,7 @@
+export const preReplies = [
+  { q: ["hi", "hello"], a: "Hey 😊 kaise ho?" },
+  { q: ["bore"], a: "Boring todte hain 😄 kuch fun baat?" },
+  { q: ["sad"], a: "Main hoon na 🤍 kya hua?" },
+  { q: ["love"], a: "Hmm 😌 dheere dheere" },
+  { q: ["kya kar rahe"], a: "Tumse baat 😉" }
+];
