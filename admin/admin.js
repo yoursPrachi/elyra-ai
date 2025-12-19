45dcb1ed1df8a2bac2a1ca37503a38ff09947a04
yoursPrachi <yogeshbalajiearth@gmail.com>
2025-12-19 23:38:23 +0530

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,13 +1,26 @@
-import { db } from "../firebase.js";
-import { collection, addDoc } from
-"https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+import { db } from "./firebase.js";
+import {
+  collection, getDocs, addDoc, deleteDoc
+} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
-window.train = async () => {
+const list = document.getElementById("list");
+
+const snap = await getDocs(collection(db, "learningQueue"));
+snap.forEach(d => {
+  list.innerHTML += `
+    <p>${d.data().question}</p>
+    <input id="${d.id}" placeholder="Answer">
+    <button onclick="save('${d.id}')">Save</button>
+    <hr>`;
+});
+
+window.save = async (id) => {
+  const val = document.getElementById(id).value;
   await addDoc(collection(db, "learnedAnswers"), {
-    question: q.value,
-    answer: a.value,
-    rating: 5,
-    trusted: true
+    q: id,
+    text: val,
+    weight: 10
   });
-  alert("Bot trained");
+  await deleteDoc(collection(db, "learningQueue", id));
+  location.reload();
 };
