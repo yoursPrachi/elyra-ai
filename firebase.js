4b4b8d80a379534cf9b20a464863dc01e58e37fc
yoursPrachi <yogeshbalajiearth@gmail.com>
2025-12-20 23:32:11 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,29 +1,19 @@
-// Firebase CDN imports
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-analytics.js";
 import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
-// Your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyCQCf2VcKQQ0C-cdon9SgSWwk0eC6Db1xk",
   authDomain: "elyra-ai-prod.firebaseapp.com",
   projectId: "elyra-ai-prod",
   storageBucket: "elyra-ai-prod.appspot.com",
   messagingSenderId: "29921626188",
-  appId: "1:29921626188:web:89b20e940c233e4d3f0ce9",
-  measurementId: "G-WPGZMCHN7M"
+  appId: "1:29921626188:web:89b20e940c233e4d3f0ce9"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-// Auth + DB
 const auth = getAuth(app);
 export const db = getFirestore(app);
 
-// Anonymous login
-signInAnonymously(auth)
-  .then(() => console.log("Firebase Auth Connected"))
-  .catch(err => console.error(err));
+// Auth ensure karne ke liye global promise
+export const authReady = signInAnonymously(auth);
